48ee6c58ed0f44a68f57e582cbcd6a26ab637e65
Ninad <95896935+Nnknoob@users.noreply.github.com>
2023-03-02 09:47:33 +0530

    Update helloworld.dart

--- helloworld.dart
+++ helloworld.dart
@@ -1,29 +1,38 @@
 import 'package:flutter/material.dart';
 
+
 void main() => runApp(MyApp());
+
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       // Application name
 
+
       title: 'Flutter Stateful Clicker Counter',
+
 
       theme: ThemeData(
         // Application theme data, you can set the colors for the application as
 
+
         // you want
+
 
         primarySwatch: Colors.orange,
       ),
+
 
       home: MyHomePage(),
     );
   }
 }
+
 
 class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
